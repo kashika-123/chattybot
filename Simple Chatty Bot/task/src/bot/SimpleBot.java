bc3dbf0f86149576d207da9195b40dfc86e99939
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-04-12 02:32:29 +0530

    Initial commit

---
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -0,0 +1,9 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+     System.out.println("Hello! My name is kashika");
+            System.out.println("I was created in 1990");
+        // write your code here
+    }
+}
