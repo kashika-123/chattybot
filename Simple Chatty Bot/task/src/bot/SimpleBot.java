beac8133fddc745337230af5cc3f1c6c49030787
kashika <kashika.tripathi_cs18@gla.ac.in>
2020-04-12 02:50:21 +0530

    creating dialouges

--- Simple Chatty Bot/task/src/bot/SimpleBot.java
+++ Simple Chatty Bot/task/src/bot/SimpleBot.java
@@ -1,9 +1,27 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-     System.out.println("Hello! My name is kashika");
-            System.out.println("I was created in 1990");
-        // write your code here
-    }
+       Scanner scanner = new Scanner(System.in);
+
+              System.out.println("Hello! My name is Aid.");
+              System.out.println("I was created in 2018.");
+              System.out.println("Please, remind me your name.");
+              String name=scanner.nextLine();
+              // reading a name
+
+              System.out.println("What a great name you have, "+name);
+              System.out.println("Let me guess your age.");
+              System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+              int remainder3=scanner.nextInt();
+              int remainder5=scanner.nextInt();
+              int remainder7=scanner.nextInt();
+              int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+              // reading all remainders
+
+              System.out.println("Your age is "+age+"; that's a good time to start programming!");
+          }
 }
